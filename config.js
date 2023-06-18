881e7ffcadbcc645f8a49fd5a1d71b2b63c78eaa
Ekaterina Lazarenko <lazarenkoea@yandex.ru>
2023-06-18 14:05:56 +0300

    port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  BASE_PATH: process.env.BASE_PATH || 'https://localhost.ru',
+  BASE_PATH: process.env.BASE_PATH || 'http://localhost.ru',
   JWT_SECRET: process.env.JWT_SECRET || 'super-secret-code',
   MONGODB_URI:
     process.env.MONGODB_URI || 'mongodb://127.0.0.1/bitfilmsdb',
